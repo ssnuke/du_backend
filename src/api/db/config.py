cffa9dc248537397e732260ff136848508741874
ssnuke <49909116+ssnuke@users.noreply.github.com>
2025-07-02 18:55:21 +0530

    URL changed

--- src/api/db/config.py
+++ src/api/db/config.py
@@ -1,3 +1,4 @@
 from decouple import config as decouple_config
 
-DATABASE_URL = decouple_config("DATABASE_URL")
+DATABASE_URL = "postgresql+psycopg://db_user:db_password@db_service:5432/dreamersunited_db"
+#decouple_config("DATABASE_URL")
